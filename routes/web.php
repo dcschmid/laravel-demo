292c4192fdd66087fc95a79cb6d787c3e0c56276
dcschmid <daniel.schmid1980@icloud.com>
2024-06-23 11:27:44 +0200

    feat: route parameters and route model binding

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
+Route::get('posts/{post}', [PostController::class, 'show'])->name('post.show');
